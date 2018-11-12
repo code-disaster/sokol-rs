3f0a6d36f4166b72a4399c4ba376d8aaac2f8ac1
Daniel Ludwig <codi@code-disaster.com>
2018-11-12 20:00:16 +0100

    sokol_app: add key codes & modifiers

--- sokol/src/app.rs
+++ sokol/src/app.rs
@@ -5,11 +5,11 @@ use std::os::raw::c_int;
 use std::os::raw::c_void;
 
 pub mod ffi {
+    use std::ffi::CStr;
     use std::ffi::CString;
     use std::os::raw::c_char;
     use std::os::raw::c_int;
     use std::os::raw::c_void;
-    use std::ffi::CStr;
 
     const SAPP_MAX_TOUCHPOINTS: usize = 8;
     const _SAPP_MAX_MOUSEBUTTONS: usize = 3;
@@ -29,9 +29,9 @@ pub mod ffi {
     struct SappEvent {
         event_type: super::SappEventType,
         frame_count: u32,
-        key_code: u32,
+        key_code: super::SappKeycode,
         char_code: u32,
-        modifiers: u32,
+        modifiers: super::SappModifier,
         mouse_button: super::SappMouseButton,
         mouse_x: f32,
         mouse_y: f32,
@@ -203,6 +203,132 @@ pub enum SappEventType {
 
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq)]
+pub enum SappKeycode {
+    KeyInvalid = 0,
+    KeySpace = 32,
+    KeyApostrophe = 39,
+    KeyComma = 44,
+    KeyMinus = 45,
+    KeyPeriod = 46,
+    KeySlash = 47,
+    Key0 = 48,
+    Key1 = 49,
+    Key2 = 50,
+    Key3 = 51,
+    Key4 = 52,
+    Key5 = 53,
+    Key6 = 54,
+    Key7 = 55,
+    Key8 = 56,
+    Key9 = 57,
+    KeySemicolon = 59,
+    KeyEqual = 61,
+    KeyA = 65,
+    KeyB = 66,
+    KeyC = 67,
+    KeyD = 68,
+    KeyE = 69,
+    KeyF = 70,
+    KeyG = 71,
+    KeyH = 72,
+    KeyI = 73,
+    KeyJ = 74,
+    KeyK = 75,
+    KeyL = 76,
+    KeyM = 77,
+    KeyN = 78,
+    KeyO = 79,
+    KeyP = 80,
+    KeyQ = 81,
+    KeyR = 82,
+    KeyS = 83,
+    KeyT = 84,
+    KeyU = 85,
+    KeyV = 86,
+    KeyW = 87,
+    KeyX = 88,
+    KeyY = 89,
+    KeyZ = 90,
+    KeyLeftBracket = 91,
+    KeyBackslash = 92,
+    KeyRightBracket = 93,
+    KeyGraveAccent = 96,
+    KeyWorld1 = 161,
+    KeyWorld2 = 162,
+    KeyEscape = 256,
+    KeyEnter = 257,
+    KeyTab = 258,
+    KeyBackspace = 259,
+    KeyInsert = 260,
+    KeyDelete = 261,
+    KeyRight = 262,
+    KeyLeft = 263,
+    KeyDown = 264,
+    KeyUp = 265,
+    KeyPageUp = 266,
+    KeyPageDown = 267,
+    KeyHome = 268,
+    KeyEnd = 269,
+    KeyCapsLock = 280,
+    KeyScrollLock = 281,
+    KeyNumLock = 282,
+    KeyPrintScreen = 283,
+    KeyPause = 284,
+    KeyF1 = 290,
+    KeyF2 = 291,
+    KeyF3 = 292,
+    KeyF4 = 293,
+    KeyF5 = 294,
+    KeyF6 = 295,
+    KeyF7 = 296,
+    KeyF8 = 297,
+    KeyF9 = 298,
+    KeyF10 = 299,
+    KeyF11 = 300,
+    KeyF12 = 301,
+    KeyF13 = 302,
+    KeyF14 = 303,
+    KeyF15 = 304,
+    KeyF16 = 305,
+    KeyF17 = 306,
+    KeyF18 = 307,
+    KeyF19 = 308,
+    KeyF20 = 309,
+    KeyF21 = 310,
+    KeyF22 = 311,
+    KeyF23 = 312,
+    KeyF24 = 313,
+    KeyF25 = 314,
+    KeyKP0 = 320,
+    KeyKP1 = 321,
+    KeyKP2 = 322,
+    KeyKP3 = 323,
+    KeyKP4 = 324,
+    KeyKP5 = 325,
+    KeyKP6 = 326,
+    KeyKP7 = 327,
+    KeyKP8 = 328,
+    KeyKP9 = 329,
+    KeyKPDecimal = 330,
+    KeyKPDivide = 331,
+    KeyKPMultiply = 332,
+    KeyKPSubtract = 333,
+    KeyKPAdd = 334,
+    KeyKPEnter = 335,
+    KeyKPEqual = 336,
+    KeyLeftShift = 340,
+    KeyLeftControl = 341,
+    KeyLeftAlt = 342,
+    KeyLeftSuper = 343,
+    KeyRightShift = 344,
+    KeyRightControl = 345,
+    KeyRightAlt = 346,
+    KeyRightSuper = 347,
+    KeyMenu = 348,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum SappMouseButton {
     Invalid = -1,
     Left = 0,
@@ -210,12 +336,21 @@ pub enum SappMouseButton {
     Middle = 2,
 }
 
+bitflags! {
+    pub struct SappModifier: u32 {
+        const Shift = 0x01;
+        const Control = 0x02;
+        const Alt = 0x04;
+        const Super = 0x08;
+    }
+}
+
 pub struct SappEvent {
     pub event_type: SappEventType,
     pub frame_count: u32,
-    pub key_code: u32,
+    pub key_code: SappKeycode,
     pub char_code: u32,
-    pub modifiers: u32,
+    pub modifiers: SappModifier,
     pub mouse_button: SappMouseButton,
     pub mouse_x: f32,
     pub mouse_y: f32,
